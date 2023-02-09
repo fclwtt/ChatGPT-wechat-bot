de7fd82c54c8e2d6a40357c87ca67a1ebabb0ac6
fclwtt <74441449+fclwtt@users.noreply.github.com>
2023-02-09 19:31:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export default {
   // 填入你的OPENAI_API_KEY
-  OPENAI_API_KEY: '',
+  OPENAI_API_KEY: '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',
   // 设置获取消息的重试次数
   retryTimes: 3,
   // 在群组中设置唤醒微信机器人的关键词
-  groupKey: '',
+  groupKey: '牛牛',
   // 在私聊中设置唤醒微信机器人的关键词
-  privateKey: '',
+  privateKey: '牛牛',
   // 重置上下文的关键词，如可设置为reset
-  resetKey: 'reset',
+  resetKey: '重置',
   // 开启会后收到ChatGPT的自动回复
   autoReply: true,
   // 根据正则匹配是否自动通过好友验证
